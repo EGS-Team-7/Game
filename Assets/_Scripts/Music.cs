e02962a47e6287849c7d5559b99b0e07221127c2
pjm349@drexel.edu <pjm349@drexel.edu>
2020-05-20 13:47:03 -0400

    Turn off play on awake for Music, fixed singleton implementation, added comments

--- Assets/_Scripts/Music.cs
+++ Assets/_Scripts/Music.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 public class Music : MonoBehaviour
 {
     AudioSource music;
+    private static Music _musicPlayer;          // Because this is _static_, it does not belong to any object
+                                                // Allowing us to detect if there are duplicates of this class
 
     private void Awake()
     {
-        DontDestroyOnLoad(gameObject);
-        music = gameObject.GetComponent<AudioSource>();
-    }
+        if (_musicPlayer)
+        {
+            // If there is already a reference to a music player,
+            // Then we need to destroy ourselves to prevent the music from
+            // playing again
 
-    void Update()
-    {
-        
+            Destroy(gameObject);
+        }
+        else
+        {
+            // There is no reference to a music player, so we must be it
+            // We set ourselves to be that reference, set ourselves to not be destroyed,
+            // And start playing our song.
+
+            _musicPlayer = this;
+            DontDestroyOnLoad(gameObject);
+            music = gameObject.GetComponent<AudioSource>();
+            if (!music.isPlaying)
+            {
+                music.Play();
+            }
+        }
     }
 }
